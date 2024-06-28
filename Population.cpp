7c478ba783f9ab057978910331a12aa7eb262304
IvanMavlikaev <zaufmam@mail.ru>
2024-06-28 17:16:22 +0300

    update Population

--- Population.cpp
+++ Population.cpp
@@ -1,7 +1,34 @@
 #include "Population.h"
 
 
-Population::Population(double down, double up, int count_individs, int count_steps, double probMutation) {
-        // здесь генерируется популяция 
+Population::Population(double down, double up, int countIndivids, int countStep, double probMutation) {
+        this->countIndivids = countIndivids;
+        for (int i = 0; i < countIndivids; i++) {
+            Chromosome new_chromosome = Chromosome(countStep, probMutation, down, up, i + 1);
+            this->chromosomes.push_back(new_chromosome);
+        }
 }
 
+void Population::updatePopulation(std::vector<Chromosome> chromosomes) {
+    this->chromosomes = {};
+    for (int i = 0; i < chromosomes.size(); i++) {
+        this->chromosomes.push_back(chromosomes[i]);
+    }
+    this->countIndivids = this->chromosomes.size();
+}
+
+void Population::selection(Polynomial *polynom, double criterion) {
+    std::vector<Chromosome> children;
+    for (int i = 0; i < this->countIndivids; i++) {
+        double difference = polynom->Evaluation(this->chromosomes[i]);
+        if (difference <= criterion) {
+            int j = i;
+            while (j = i) {
+                j = rand() % (countIndivids);
+            }
+            std::pair<Chromosome> pair_children = this->chromosomes[i]->recombination(this->chromosomes[i], this->chromosomes[j]);
+            children.push_back(pair_children[0]);
+            children.push_back(pair_children[1]);
+        }
+    }
+}
--- Population.h
+++ Population.h
@@ -2,16 +2,18 @@
 #define Population_h
 #include <iostream>
 #include <vector>
+#include <random>
 #include "Polynomial.h"
 
 class Population{
     public:
-        int count_individs;
-        std::vector<Chromosome> chromosomes;
+        int countIndivids = 0;
+        std::vector<Chromosome> chromosomes = {};
         
-        Population(double down = 0, double up = 100, int count_individs = 50, int count_steps = 10, double probMutation = 0.1); // генерируется популяция из count_individs особей 
-        //Population updatePopulation();
-        //Population selection();
+        Population(double down = 0, double up = 100, int countIndivids = 50, int countSteps = 10, double probMutation = 0.1); // генерируется популяция из count_individs особей 
+        Population updatePopulation(std::vector<Chromosome> chromosomes);
+        //реализовать ЭЛИТАРНЫЙ ОТБОР
+        Population selection(Polynomial * polynom, double criterion);
 };
 
 #endif
